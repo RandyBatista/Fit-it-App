c75c90a9601eedce47d534bc9c0157feff8f06fc
hoj2atwit <43070855+hoj2atwit@users.noreply.github.com>
2022-07-25 17:52:36 -0400

    "I Know My Size" Button now directs to size input fragment

--- app/src/main/java/edu/wit/mobileapp/fit_it_app/ChooseSizeInputFragment.java
+++ app/src/main/java/edu/wit/mobileapp/fit_it_app/ChooseSizeInputFragment.java
@@ -7,12 +7,26 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class ChooseSizeInputFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.get_measures_frag, container, false);
+
+        Button measureBtn = rootView.findViewById(R.id.measurements_Btn);
+        Button sizeBtn = rootView.findViewById(R.id.size_Btn);
+
+        sizeBtn.setOnClickListener(v ->{
+            FragmentManager fm = getActivity().getSupportFragmentManager();
+            FragmentTransaction transaction = fm.beginTransaction();
+            Fragment fragment = new SizeInputFragment();
+            transaction.replace(R.id.content, fragment);
+            transaction.commit();
+        });
+
         return rootView;
     }
 }
--- app/src/main/java/edu/wit/mobileapp/fit_it_app/SizeInputFragment.java
+++ app/src/main/java/edu/wit/mobileapp/fit_it_app/SizeInputFragment.java
@@ -4,21 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 public class SizeInputFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.log_in_fragment, container, false);
-        Button registerBtn = rootView.findViewById(R.id.register_btn);
-        Button guestBtn = rootView.findViewById(R.id.guest_btn);
-        Button submitBtn = rootView.findViewById(R.id.submit_btn);
+        View rootView = inflater.inflate(R.layout.specify_size_fragment, container, false);
         return rootView;
     }
 }
