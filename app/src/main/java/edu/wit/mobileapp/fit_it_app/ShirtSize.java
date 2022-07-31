89db8771baed1a2c3ed7ca72f0dbc0536e4d9d2d
hoj2atwit <johnniezho@gmail.com>
2022-07-30 21:50:24 -0400

    Added sizes in usable dictionaries.

---
+++ app/src/main/java/edu/wit/mobileapp/fit_it_app/ShirtSize.java
@@ -0,0 +1,71 @@
+package edu.wit.mobileapp.fit_it_app;
+
+import java.util.HashMap;
+
+public class ShirtSize {
+
+    Size bust;
+    Size waist;
+    Size hip;
+
+    public ShirtSize(Size bust, Size waist, Size hip){
+        this.bust = bust;
+        this.waist = waist;
+        this.hip = hip;
+    }
+
+    public HashMap<String, ShirtSize> getUniNike(){
+        HashMap<String, ShirtSize> sizes = new HashMap<>();
+        sizes.put("XXS", new ShirtSize(new Size(72, 80), new Size(57, 65), new Size(72, 80)));
+        sizes.put("XS", new ShirtSize(new Size(80, 88), new Size(65, 73), new Size(80, 88)));
+        sizes.put("S", new ShirtSize(new Size(88, 96), new Size(73, 81), new Size(88, 96)));
+        sizes.put("M", new ShirtSize(new Size(96, 104), new Size(81, 89), new Size(96, 104)));
+        sizes.put("L", new ShirtSize(new Size(104, 112), new Size(89, 97), new Size(104, 112)));
+        sizes.put("XL", new ShirtSize(new Size(112, 124), new Size(97, 109), new Size(112, 120)));
+        sizes.put("2XL", new ShirtSize(new Size(124, 136), new Size(109, 121), new Size(120, 128)));
+        sizes.put("3XL", new ShirtSize(new Size(136, 148), new Size(121, 133), new Size(128, 136)));
+        sizes.put("4XL", new ShirtSize(new Size(148, 160), new Size(133, 145), new Size(136, 148)));
+        return sizes;
+    }
+    public HashMap<String, ShirtSize> getWomenNike(){
+        HashMap<String, ShirtSize> sizes = new HashMap<>();
+        sizes.put("XXS", new ShirtSize(new Size(70, 76), new Size(54, 60), new Size(78, 84)));
+        sizes.put("XS", new ShirtSize(new Size(76, 83), new Size(60, 67), new Size(84, 91)));
+        sizes.put("S", new ShirtSize(new Size(83, 90), new Size(67, 74), new Size(91, 98)));
+        sizes.put("M", new ShirtSize(new Size(90, 97), new Size(74, 81), new Size(98, 105)));
+        sizes.put("L", new ShirtSize(new Size(97, 104), new Size(81, 88), new Size(105, 112)));
+        sizes.put("XL", new ShirtSize(new Size(104, 114), new Size(88, 98), new Size(112, 120)));
+        sizes.put("2XL", new ShirtSize(new Size(114, 124), new Size(98, 108), new Size(120, 128)));
+        sizes.put("1X", new ShirtSize(new Size(114, 124), new Size(104, 114), new Size(118, 128)));
+        sizes.put("2X", new ShirtSize(new Size(124, 134), new Size(114, 124), new Size(128, 138)));
+        sizes.put("3X", new ShirtSize(new Size(134, 144), new Size(124, 134), new Size(138, 148)));
+        return sizes;
+    }
+    public HashMap<String, ShirtSize> getBoyNike(){
+        HashMap<String, ShirtSize> sizes = new HashMap<>();
+        sizes.put("XS", new ShirtSize(new Size(64.5, 66), new Size(59.5, 61.5), new Size(68.5, 71)));
+        sizes.put("S", new ShirtSize(new Size(66, 69), new Size(61.5, 65), new Size(71, 74.5)));
+        sizes.put("S+", new ShirtSize(new Size(70.5, 76.5), new Size(65.5, 70.5), new Size(75.5, 81)));
+        sizes.put("M", new ShirtSize(new Size(69, 75), new Size(65, 69), new Size(74.5, 79.5)));
+        sizes.put("M+", new ShirtSize(new Size(76.5, 83.5), new Size(70.5, 76), new Size(81, 87)));
+        sizes.put("L", new ShirtSize(new Size(75, 81.5), new Size(69, 72.5), new Size(79.5, 84.5)));
+        sizes.put("L+", new ShirtSize(new Size(83.5, 91), new Size(76, 82), new Size(87, 93.5)));
+        sizes.put("XL", new ShirtSize(new Size(81.5, 88.5), new Size(72.5, 75.5), new Size(84.5, 89.5)));
+        sizes.put("XL+", new ShirtSize(new Size(91, 98), new Size(82, 88), new Size(93.5, 100)));
+        return sizes;
+    }
+    public HashMap<String, ShirtSize> getGirlNike(){
+        HashMap<String, ShirtSize> sizes = new HashMap<>();
+        sizes.put("XS", new ShirtSize(new Size(64.5, 68), new Size(59.5, 61), new Size(68.5, 73)));
+        sizes.put("S", new ShirtSize(new Size(68, 73), new Size(61, 64), new Size(73, 78.5)));
+        sizes.put("S+", new ShirtSize(new Size(73.5, 80.5), new Size(67.5, 74.5), new Size(79.5, 85.5)));
+        sizes.put("M", new ShirtSize(new Size(73, 79), new Size(64, 68), new Size(78.5, 83.5)));
+        sizes.put("M+", new ShirtSize(new Size(80.5, 87.5), new Size(74.5, 82), new Size(85.5, 92)));
+        sizes.put("L", new ShirtSize(new Size(79, 85.5), new Size(68, 71.5), new Size(83.5, 88.5)));
+        sizes.put("L+", new ShirtSize(new Size(87.5, 96), new Size(82, 90), new Size(92, 99)));
+        sizes.put("XL", new ShirtSize(new Size(85.5, 92.5), new Size(71.5, 74.5), new Size(88.5, 93.5)));
+        sizes.put("XL+", new ShirtSize(new Size(96, 105), new Size(90, 99), new Size(99, 107)));
+        return sizes;
+    }
+
+}
