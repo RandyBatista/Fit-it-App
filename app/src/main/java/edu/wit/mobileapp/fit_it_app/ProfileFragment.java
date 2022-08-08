8affe1f5eacc338245fdab0d9f0a566da49f5099
phant9atwit <phant9@wit.edu>
2022-08-07 23:01:31 -0400

    Added profile fragment and layout

---
+++ app/src/main/java/edu/wit/mobileapp/fit_it_app/ProfileFragment.java
@@ -0,0 +1,23 @@
+package edu.wit.mobileapp.fit_it_app;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ProfileFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.profile_fragment, container, false);
+
+
+
+        return rootView;
+    }
+}
